305a52ec1567b7b7b45e838b53b2c931e497e34e
throwmen <donjuanchus@gmail.com>
2024-10-17 07:50:03 -0500

    sidebar fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Sidebar } from './components/Sidebar';
-import { ErrorComponent } from './components/ErrorComponent';
-import { Inicio } from './components/pages/Inicio';
-import { LogIn } from './components/pages/LogIn';
-import { Datos } from './components/pages/Datos';
-import { HelpButton } from './components/Help';
-import { FileDetails } from './components/pages/Datos2';
-import {Pronosticos} from './components/pages/Pronosticos';
-import {Costos} from './components/pages/Costos';
-import {Escenarios} from './components/pages/Escenarios';
-import { Navbar } from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Sidebar } from "./components/Sidebar";
+import { ErrorComponent } from "./components/ErrorComponent";
+import { Inicio } from "./components/pages/Inicio";
+import { LogIn } from "./components/pages/LogIn";
+import { Datos } from "./components/pages/Datos";
+import { HelpButton } from "./components/Help";
+import { FileDetails } from "./components/pages/Datos2";
+import { Pronosticos } from "./components/pages/Pronosticos";
+import { Costos } from "./components/pages/Costos";
+import { Escenarios } from "./components/pages/Escenarios";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route 
-          path="/" 
-          element={
-              <LogIn/>
-          }
-        />
+        <Route path="/" element={<LogIn />} />
         {/* Rutas con la Sidebar y Navbar */}
-        <Route 
+        <Route
           path="/file/:fileName"
           element={
-            <div className="flex h-screen">
+            <div className="flex h-full">
               <Sidebar />
               <HelpButton />
               <div className="flex flex-col flex-1">
@@ -35,16 +30,16 @@ function App() {
                 </div>
               </div>
             </div>
-          } 
+          }
         />
-        <Route 
-          path="/inicio" 
+        <Route
+          path="/inicio"
           element={
-            <div className="flex h-screen">
+            <div className="flex h-full">
               <Sidebar />
               <HelpButton />
               <div className="flex flex-col flex-1">
-                <Navbar /> 
+                <Navbar />
                 <div className="flex-1 p-4">
                   <Inicio />
                 </div>
@@ -52,58 +47,58 @@ function App() {
             </div>
           }
         />
-        <Route 
-          path="/datos" 
+        <Route
+          path="/datos"
           element={
-            <div className="flex h-screen">
+            <div className="flex h-full">
               <Sidebar />
               <div className="flex flex-col flex-1">
                 <Navbar />
                 <div className="flex-1 p-4">
                   <Datos />
+                </div>
+              </div>
+              <HelpButton />
             </div>
-          </div>
-          <HelpButton />
-        </div>
           }
         />
-        <Route 
-          path="/pronosticos" 
+        <Route
+          path="/pronosticos"
           element={
-            <div className="flex h-screen">
+            <div className="flex h-full">
               <Sidebar />
               <div className="flex flex-col flex-1">
                 <Navbar />
                 <div className="flex-1 p-4">
                   <Pronosticos />
+                </div>
+              </div>
+              <HelpButton />
             </div>
-          </div>
-          <HelpButton />
-        </div>
           }
         />
-        <Route 
-          path="/costos" 
+        <Route
+          path="/costos"
           element={
-            <div className="flex h-screen">
+            <div className="flex h-full">
               <Sidebar />
               <div className="flex flex-col flex-1">
                 <Navbar />
                 <div className="flex-1 p-4">
                   <Costos />
+                </div>
+              </div>
+              <HelpButton />
             </div>
-          </div>
-          <HelpButton />
-        </div>
           }
         />
-        <Route 
-          path="/escenarios" 
+        <Route
+          path="/escenarios"
           element={
-            <div className="flex">
+            <div className="flex h-full">
               <Sidebar />
               <div className="flex flex-col flex-1">
-                <Navbar /> 
+                <Navbar />
                 <div className="flex-1 p-4">
                   <Escenarios />
                 </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,49 +1,70 @@
 import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
-    return (
-        <div className="h-full w-64 bg-gray-100 text-gray-800 flex flex-col">
-            {/* Logo */}
-            <div className="flex items-center justify-center h-20 my-5">
-                <div className="w-50 h-50 flex items-center justify-center">
-                    <img src="src/Images/Logo.png" alt="Logo" className="w-48 h-48" draggable="false" />
-                </div>
-            </div>
-
-            {/* Menú */}
-            <nav className="flex-1 px-4 py-8 overflow-y-auto">
-                <ul className="space-y-4">
-                    {/* Inicio */}
-                    <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
-                        <span className="text-2xl">🏠</span>
-                        <Link to="/inicio" className="text-gray-800 hover:text-gray-400">Inicio</Link>
-                    </li>
-
-                    {/* Banco de datos */}
-                    <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
-                        <span className="text-2xl">🗄️</span>
-                        <Link to="/datos" className="text-gray-800 hover:text-gray-400">Banco de datos</Link>
-                    </li>
-
-                    {/* Pronósticos */}
-                    <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
-                        <span className="text-2xl">📊</span>
-                        <Link to="/pronosticos" className="text-gray-800 hover:text-gray-400">Pronósticos</Link>
-                    </li>
-
-                    {/* Análisis de costos */}
-                    <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
-                        <span className="text-2xl">💰</span>
-                        <Link to="/costos" className="text-gray-800 hover:text-gray-400">Análisis de costos</Link>
-                    </li>
-
-                    {/* Análisis de escenario */}
-                    <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
-                        <span className="text-2xl">📋</span>
-                        <Link to="/escenarios" className="text-gray-800 hover:text-gray-400">Análisis de escenario</Link>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <div className="w-64 bg-gray-100 text-gray-800 flex flex-col">
+      {/* Logo */}
+      <div className="flex items-center justify-center h-20 my-5">
+        <div className="w-50 h-50 flex items-center justify-center">
+          <img
+            src="src/Images/Logo.png"
+            alt="Logo"
+            className="w-48 h-48"
+            draggable="false"
+          />
         </div>
-    );
+      </div>
+
+      {/* Menú */}
+      <nav className="flex-1 px-4 py-8 overflow-y-auto">
+        <ul className="space-y-4">
+          {/* Inicio */}
+          <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+            <span className="text-2xl">🏠</span>
+            <Link to="/inicio" className="text-gray-800 hover:text-gray-400">
+              Inicio
+            </Link>
+          </li>
+
+          {/* Banco de datos */}
+          <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+            <span className="text-2xl">🗄️</span>
+            <Link to="/datos" className="text-gray-800 hover:text-gray-400">
+              Banco de datos
+            </Link>
+          </li>
+
+          {/* Pronósticos */}
+          <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+            <span className="text-2xl">📊</span>
+            <Link
+              to="/pronosticos"
+              className="text-gray-800 hover:text-gray-400"
+            >
+              Pronósticos
+            </Link>
+          </li>
+
+          {/* Análisis de costos */}
+          <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+            <span className="text-2xl">💰</span>
+            <Link to="/costos" className="text-gray-800 hover:text-gray-400">
+              Análisis de costos
+            </Link>
+          </li>
+
+          {/* Análisis de escenario */}
+          <li className="flex items-center space-x-3 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+            <span className="text-2xl">📋</span>
+            <Link
+              to="/escenarios"
+              className="text-gray-800 hover:text-gray-400"
+            >
+              Análisis de escenario
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
